1b6349d3dc5c665e2148e2e1d993586053a8c7be
sare <aboudou.samadou.sare@gmail.com>
2024-10-12 20:06:11 +0200

    feat: add typewriter

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,20 @@
+import { Flex, Text } from "@chakra-ui/react";
+import Typewriter from "typewriter-effect";
+
 const Home = () => {
-  return <></>;
+  return (
+    <Flex h="100vh" py={[10, 10]} padding={10} justifyContent="center">
+      <Text fontSize="4xl">
+        <Typewriter
+          options={{
+            strings: ["Easy Billing"],
+            autoStart: true,
+            loop: true,
+          }}
+        />
+      </Text>
+    </Flex>
+  );
 };
 
 export default Home;
